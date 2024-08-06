02a01b84b2ec57fac181767d8e79590bea592ccb
Gianmarco Fraccaroli <gianmarco.fraccaroli@gmail.com>
2024-08-06 23:32:49 +0200

    minors

--- check/src/checks/mod.rs
+++ check/src/checks/mod.rs
@@ -10,23 +10,24 @@ pub mod inflation;
 
 pub trait DoCheck {
     async fn check(sdk: &Sdk, state: &mut crate::state::State) -> Result<(), String>;
+
     async fn do_check(sdk: &Sdk, state: &mut crate::state::State) -> Result<(), String> {
-        let mut times = 3;
-        while times > 0 {
+        let mut times = 0;
+        while times <= 3 {
             let result = Self::check(sdk, state).await;
             if result.is_ok() {
                 return result
             } else {
-                if times == 1 {
+                if times == 3 {
                     tracing::info!("Check {} failed {} times, returning error", Self::to_string(), times);
                     return result
                 }
-                tracing::info!("Check {} failed retrying ({}/{})...", Self::to_string(), 3 - times, 3);
-                times = times - 1;
+                tracing::info!("Check {} failed retrying ({}/{})...", Self::to_string(), times, 3);
+                times = times + 1;
                 sleep(Duration::from_secs(1)).await
             }
         }
-        Err(format!("Failed {} check", Self::to_string()))
+        Err(format!("Failed {} check (end)", Self::to_string()))
     }
     
     fn to_string() -> String;
