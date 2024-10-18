ba843e3f6c33dd73c6df5aad6210a17ace1906ec
Gianmarco Fraccaroli <gianmarco.fraccaroli@gmail.com>
2024-10-18 15:17:33 +0200

    update check

--- check/src/state.rs
+++ check/src/state.rs
@@ -3,7 +3,6 @@ use namada_sdk::token;
 #[derive(Clone, Debug, Default)]
 pub struct State {
     pub last_block_height: u64,
-    pub total_times_block_query_was_equal: u64,
     pub last_epoch: u64,
     pub last_total_supply: token::Amount,
 }
@@ -12,7 +11,6 @@ impl State {
     pub fn from_height(height: u64) -> Self {
         Self {
             last_block_height: height,
-            total_times_block_query_was_equal: 0,
             last_epoch: 0,
             last_total_supply: token::Amount::default(),
         }
