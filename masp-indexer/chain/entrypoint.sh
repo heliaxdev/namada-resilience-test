0d1553f563d0c682691cdb7e54a0ed99e7afa057
Gianmarco Fraccaroli <gianmarco.fraccaroli@gmail.com>
2024-11-07 11:35:04 +0100

    update masp build command

--- masp-indexer/chain/entrypoint.sh
+++ masp-indexer/chain/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 set -e
 echo "entered entrypoint"
-until pg_isready -h postgres -p 5432 | grep 'accepting connections'; do
+until pg_isready -h 30.0.0.21 -p 5432 | grep 'accepting connections'; do
   echo "Waiting for PostgreSQL to be ready..."
   sleep 3  # Wait for 3 seconds before checking again
 done
