0b89c1f533aee73771839e63bd80b97906fa7df4
Gianmarco Fraccaroli <gianmarco.fraccaroli@gmail.com>
2024-11-07 10:20:16 +0100

    added entrypoint for masp images

---
+++ masp-indexer/block-filter/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+set -e
+echo "entered entrypoint"
+until pg_isready -h postgres -p 5432 | grep 'accepting connections'; do
+  echo "Waiting for PostgreSQL to be ready..."
+  sleep 3  # Wait for 3 seconds before checking again
+done
+echo "PostgreSQL is now accepting connections!"
+
+./block-index
