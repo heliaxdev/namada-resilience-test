79dba8e2e34b89891b7a9d8ecb3dcf0e1d4637e2
Gianmarco Fraccaroli <gianmarco.fraccaroli@gmail.com>
2024-11-15 16:17:53 +0100

    workload wait for masp

--- workload/scripts/init_script.sh
+++ workload/scripts/init_script.sh
@@ -22,12 +22,12 @@ done
 CHAIN_ID=$(basename $CHAIN_ID)
 
 # Wait for the JSON RPC to come up for masp indexer
-# json_rpc_ready=0
-# while [ $json_rpc_ready -eq 0 ]
-# do
-#     json_rpc_ready=$(curl -I ${MASP_INDEXER_URL}/api/v1/health | grep 200 | wc -l)
-#     sleep 5
-# done
+json_rpc_ready=0
+while [ $json_rpc_ready -eq 0 ]
+do
+    json_rpc_ready=$(curl -I http:://30.0.0.20:5000/api/v1/health | grep 200 | wc -l)
+    sleep 5
+done
 
 # Ready to start workload
 echo "Ready to start the workload"
