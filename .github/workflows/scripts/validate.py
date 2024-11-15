0241b71890b6e773105449f879ff9e6a9c239bbe
Gianmarco Fraccaroli <gianmarco.fraccaroli@gmail.com>
2024-11-15 11:10:34 +0100

    improve validate ci

--- .github/workflows/scripts/validate.py
+++ .github/workflows/scripts/validate.py
@@ -1,33 +1,30 @@
 import subprocess
 import yaml
+import os
 
-image_map = {
-    "namada-genesis": "main",
-    "namada": "main",
-    "namada": "main",
-    "namada": "main",
-    "namada": "main",
-    "workload": "latest",
-    "workload": "latest",
-    "workload": "latest",
+default_image_map = {
+    "namada-genesis": os.environ.get("NAMADA_TAG", "main"),
+    "namada": os.environ.get("NAMADA_TAG", "main"),
+    "workload": os.environ.get("WORKLOAD_TAG", "latest"),
     "check": "latest",
-    "masp-indexer-chain": "latest",
-    "masp-indexer-webserver": "latest",
-    "masp-indexer-block-filter": "latest",
+    "masp-indexer-chain": os.environ.get("MASP_TAG", "latest"),
+    "masp-indexer-webserver": os.environ.get("MASP_TAG", "latest"),
+    "masp-indexer-block-filter": os.environ.get("MASP_TAG", "latest"),
 }
 
+print("Using the following tags:")
+print(default_image_map)
 
 with open("config/docker-compose.yml", 'r') as file:
     current_docker_compose = yaml.safe_load(file)
 
 for service in current_docker_compose['services']:
     current_image = current_docker_compose['services'][service]['image'].split(':')[0]
-    if current_image in image_map:
-        print(current_image)
-        tag = image_map[current_image]
+    if current_image in default_image_map:
+        tag = default_image_map[current_image]
         updated_image = "us-central1-docker.pkg.dev/molten-verve-216720/heliax-repository/{}:{}".format(current_image, tag)
         current_docker_compose['services'][service]['image'] = updated_image
-        # subprocess.run(["docker", "pull", updated_image])
+
 
 updated_docker_compose_path = "config/docker-compose-test.yml"
 with open(updated_docker_compose_path, 'w') as outfile:
