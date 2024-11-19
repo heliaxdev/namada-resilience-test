7e2cf4c460bb2c25be359429fec22a5a5cbc4cc2
Gianmarco Fraccaroli <gianmarco.fraccaroli@gmail.com>
2024-11-19 13:28:35 +0100

    improve ci

--- .github/workflows/scripts/validate.py
+++ .github/workflows/scripts/validate.py
@@ -24,7 +24,7 @@ for service in current_docker_compose['services']:
         tag = default_image_map[current_image]
         updated_image = "ghcr.io/heliaxdev/ant-{}:{}".format(current_image, tag)
         current_docker_compose['services'][service]['image'] = updated_image
-        subprocess.run(["docker", "pull", updated_image]) 
+        # subprocess.run(["docker", "pull", updated_image]) 
 
 
 updated_docker_compose_path = "config/docker-compose-test.yml"
@@ -32,4 +32,4 @@ with open(updated_docker_compose_path, 'w') as outfile:
     yaml.dump(current_docker_compose, outfile)
 
 
-# subprocess.run(["docker-compose", "-f", updated_docker_compose_path, "up"]) 
+subprocess.run(["docker-compose", "-f", updated_docker_compose_path, "up"]) 
