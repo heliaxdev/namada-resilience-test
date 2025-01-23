bb45e6928d4ea329241be30a4796a2a884517d4e
Gianmarco Fraccaroli <gianmarco.fraccaroli@gmail.com>
2025-01-23 10:59:27 +0100

    minors

---
+++ clean.sh
@@ -0,0 +1,17 @@
+rm -rf config/validator-0
+rm -rf config/validator-1
+rm -rf config/validator-2
+rm -rf config/fullnode
+rm -rf config/container_ready
+
+mkdir -p config/validator-0
+mkdir -p config/validator-1
+mkdir -p config/validator-2
+mkdir -p config/fullnode
+mkdir -p config/container_ready
+
+touch config/validator-0/DO_NOT_REMOVE
+touch config/validator-1/DO_NOT_REMOVE
+touch config/validator-2/DO_NOT_REMOVE
+touch config/fullnode/DO_NOT_REMOVE
+touch config/container_ready/DO_NOT_REMOVE
