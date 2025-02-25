a1396d712990b1f1386a394c5eb2fca6e48e0c07
yito88 <yuji@phact-columba.com>
2025-02-25 23:33:22 +0100

    add new file

---
+++ workload/src/check/vote_result.rs
@@ -0,0 +1,49 @@
+use serde_json::json;
+use typed_builder::TypedBuilder;
+
+use crate::check::{CheckContext, CheckInfo};
+use crate::executor::StepError;
+use crate::sdk::namada::Sdk;
+use crate::types::{Alias, ProposalId, ProposalVote};
+use crate::utils::{get_vote_results, RetryConfig};
+
+#[derive(TypedBuilder)]
+pub struct VoteResult {
+    source: Alias,
+    proposal_id: ProposalId,
+    vote: ProposalVote,
+}
+
+impl CheckContext for VoteResult {
+    fn summary(&self) -> String {
+        format!("vote-result/{}", self.source.name)
+    }
+
+    async fn do_check(
+        &self,
+        sdk: &Sdk,
+        check_info: CheckInfo,
+        retry_config: RetryConfig,
+    ) -> Result<(), StepError> {
+        let votes = get_vote_results(sdk, &self.source, self.proposal_id, retry_config).await?;
+
+        let is_valid_vote = votes.iter().all(|v| *v == self.vote);
+
+        let details = json!({
+            "target_alias": self.source,
+            "proposal_id": self.proposal_id,
+            "vote": self.vote,
+            "execution_height": check_info.execution_height,
+            "check_height": check_info.check_height
+        });
+
+        antithesis_sdk::assert_always!(is_valid_vote, "Vote was accepted as expected", &details);
+
+        if is_valid_vote {
+            Ok(())
+        } else {
+            tracing::error!("{}", details);
+            Err(StepError::StateCheck(format!("VoteResult check error: Vote result {votes:?} doesn't correspond to the expected vote {}", self.vote)))
+        }
+    }
+}
