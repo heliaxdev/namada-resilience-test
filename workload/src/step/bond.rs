c6faab1f03808524c33ce680d8588efa0872abd6
yito88 <yito88@protonmail.com>
2025-03-20 23:18:39 +0100

    avoid BecomeValidator failure

--- workload/src/step/bond.rs
+++ workload/src/step/bond.rs
@@ -26,8 +26,9 @@ impl StepContext for Bond {
     }
 
     async fn build_task(&self, sdk: &Sdk, state: &State) -> Result<Vec<Task>, StepError> {
+        let black_list = state.bonds.keys().cloned().collect();
         let source_account = state
-            .random_account_with_min_balance(vec![], MIN_TRANSFER_BALANCE)
+            .random_account_with_min_balance(black_list, MIN_TRANSFER_BALANCE)
             .ok_or(StepError::BuildTask("No more accounts".to_string()))?;
         let amount_account = state.get_balance_for(&source_account.alias);
         let amount = utils::random_between(1, amount_account / MAX_BATCH_TX_NUM);
