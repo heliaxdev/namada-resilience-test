f5864bdf587e60203a35df47efa1c182aa7b2733
yito88 <yito88@protonmail.com>
2025-06-02 21:59:25 +0200

    new images

--- local.sh
+++ local.sh
@@ -2,12 +2,12 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-#NAMADA_GENESIS_IMAGE="ghcr.io/heliaxdev/ant-namada-genesis:main" \
-#NAMADA_IMAGE="ghcr.io/heliaxdev/ant-namada:main" \
-#MASP_INDEXER_IMAGE_PREFIX="ghcr.io/heliaxdev/ant-masp-indexer" \
+#NAMADA_GENESIS_IMAGE="ghcr.io/heliaxdev/nrt-namada-genesis:main" \
+#NAMADA_IMAGE="ghcr.io/heliaxdev/nrt-namada:main" \
+#MASP_INDEXER_IMAGE_PREFIX="ghcr.io/heliaxdev/nrt-masp-indexer" \
 #MASP_INDEXER_IMAGE_TAG="master" \
-#WORKLOAD_IMAGE="ghcr.io/heliaxdev/ant-workload:master" \
-#CHECK_IMAGE="ghcr.io/heliaxdev/ant-check:latest" \
+#WORKLOAD_IMAGE="ghcr.io/heliaxdev/nrt-workload:master" \
+#CHECK_IMAGE="ghcr.io/heliaxdev/nrt-check:latest" \
 #WORKLOAD_NUM=3 \
 WORKLOAD_IMAGE="local-workload:latest" \
 CHECK_IMAGE="local-check:latest" \
